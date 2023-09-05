044eb39cabb076983e8d71fea0a2065286f43fc5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-05 17:57:32 +0100

    setup react-router funcionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './assets/components/Header';
+import Rocket from './assets/components/Rockets';
 
 const App = () => (
   <>
     <Header />
+    <Routes>
+      <Route path="/" element={Rocket} />
+      {/* <Route path='mission' element={ } />
+      <Route path='profile' element={ } /> */}
+    </Routes>
   </>
 );
 
