5ca2a2f7e8ac04e2c671f9a1222f288a218d17cb
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-06 03:05:31 +0100

    create basic structure for rockets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './assets/components/Header';
 import Rocket from './assets/components/Rockets';
+import Wrong from './assets/components/Wrong';
 
 const App = () => (
   <>
     <Header />
     <Routes>
-      <Route path="/" element={Rocket} />
+      <Route path="/" element={<Rocket />} />
+      <Route path="rocket" element={<Rocket />} />
+      <Route path="/*" element={<Wrong />} />
       {/* <Route path='mission' element={ } />
       <Route path='profile' element={ } /> */}
     </Routes>
---
+++ src/assets/redux/rockets/rocketSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  reducers: {
+
+  },
+  extraReducers: {
+
+  }
+});
+
+export default rocketsSlice.reducer;
