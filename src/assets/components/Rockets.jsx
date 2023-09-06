6561a08664a5b6b85243854e6caf877626a90b8c
Graciano1997 <gracianomanuelhenrique@gmail.com>
2023-09-06 09:02:30 +0100

    add fetch rocket funcionality

--- src/assets/components/Rockets.jsx
+++ src/assets/components/Rockets.jsx
@@ -1,7 +1,25 @@
-const Rocket = () => (
-  <div>
-    <h1>Rocket</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketsSlice';
+
+function Rocket() {
+  const { rocketsArray } = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRockets());
+  }, [dispatch]);
+
+  if (rocketsArray.length !== 0) {
+    
+  }
+
+  return (
+    <div>
+      {if rocketsArray.forEach((rocket) => {
+        <div>{rocket.id}</div>;
+      })}
+    </div>
+  );
+}
 
 export default Rocket;
